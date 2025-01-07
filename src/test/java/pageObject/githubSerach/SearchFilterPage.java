b285754ac4773cc96a44e162e3b46deddc317024
T.K. <150078127+tzippykovalsky@users.noreply.github.com>
2025-01-07 15:27:52 +0200

    Update SearchFilterPage.java

--- src/test/java/pageObject/githubSerach/SearchFilterPage.java
+++ src/test/java/pageObject/githubSerach/SearchFilterPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import pageObject.WebBasePage;
-
+//להעביר את כל הpackage לmain
 public class SearchFilterPage extends WebBasePage {
     public SearchFilterPage(WebDriver driver) {
         super(driver);
